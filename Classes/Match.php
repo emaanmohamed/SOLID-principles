e4c0b63c05c48fb75ea8ab43fff08b95b773fab8
emaanmohamed <eman.mohamed.ka@gmail.com>
2020-09-30 13:53:29 +0200

    single responsibility principle

---
+++ Classes/Match.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Classes;
+
+
+class Match
+{
+    private $attack;
+    private $defense;
+    private $keeper;
+
+    public function __construct()
+    {
+        $this->attack = new Attack();
+        $this->defense = new Defense();
+        $this->keeper = new Keeper();
+    }
+
+    public function start()
+    {
+        echo "start play";
+    }
+
+
+}
